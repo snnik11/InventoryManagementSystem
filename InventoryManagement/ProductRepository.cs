d1f90b3deb99b9a63c8a94c4b6ff2355fbc89f13
Nikita Soni <48999243+snnik11@users.noreply.github.com>
2023-11-03 18:38:34 +1100

    added logic for fetching files

---
+++ InventoryManagement/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanyPieShop.InventoryManagement
+{
+    internal class ProductRepository
+    {
+        private string directory = @"C:\Nikita\Code\InventoryManagementSystem\";
+        private string productFileName = "product.txt"; 
+
+        //check for file
+        private void CheckForFile()
+        {
+            string path = $"{directory}{productFileName}";
+
+            bool existingFileFound = File.Exists(path);
+            if(!existingFileFound)
+            {
+                //Create a new directory
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(directory);
+
+                //Create empty file
+                using FileStream fs = File.Create(path);
+            }
+
+        }
+
+        //public List<Domain.ProducManagement.Product> LoadProductFromFile()
+        //{
+        //    List<Domain.ProducManagement.Product> products = new List<Product>();
+
+        //}
+
+    }
+}
