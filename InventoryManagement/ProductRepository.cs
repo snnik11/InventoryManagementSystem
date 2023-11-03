a58feeb9d3de1c3eed645ca59301aff430d642aa
Nikita Soni <48999243+snnik11@users.noreply.github.com>
2023-11-03 22:39:31 +1100

    added product repo

--- InventoryManagement/ProductRepository.cs
+++ InventoryManagement/ProductRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BethanyPieShop.InventoryManagement.Domain.General;
+using BethanyPieShop.InventoryManagement.Domain.ProducManagement;
+using System;
 using System.Collections.Generic;
 using System.IO.Enumeration;
 using System.Linq;
@@ -30,11 +32,78 @@ namespace BethanyPieShop.InventoryManagement
 
         }
 
-        //public List<Domain.ProducManagement.Product> LoadProductFromFile()
-        //{
-        //    List<Domain.ProducManagement.Product> products = new List<Product>();
+        public List<Domain.ProducManagement.Product> LoadProductsFromFile()
+        {
+            List<Domain.ProducManagement.Product> products = new List<Domain.ProducManagement.Product>();
+            string path = $"{directory}{productFileName}";
+            try
+            {
+                CheckForFile();
+                string[] productAsString = File.ReadAllLines(path);
+                for (int i = 0; i < productAsString.Length; i++)
+                {
+                    string[] productSplits = productAsString[i].Split(';');
+                    bool success = int.TryParse(productSplits[0],
+                        out int productId);
+                    if (!success)
+                    {
+                        productId = 0; //set default value
+                    }
+                    string name = productSplits[1];
+                    string description = productSplits[2];
 
-        //}
+                    success = int.TryParse(productSplits[3],
+                        out int maxItemInStock);
+
+                    if(!success)
+                    {
+                        maxItemInStock = 100;
+                    }
+
+                    success = int.TryParse(productSplits[4],
+                        out int itemPrice);
+
+                    if (!success)
+                    {
+                        itemPrice = 0;
+                    }
+
+                    success = Enum.TryParse(productSplits[5],
+                        out Currency currency);
+
+                   if (!success)
+                    {
+                        currency = Currency.Dollar;
+                    }
+
+                    success = Enum.TryParse(productSplits[6],
+                        out UnitType unitType);
+
+                    string productType = productSplits[7];
+
+                    Product product = null;
+
+                    switch (productType)
+                    {
+                        case "1":
+                            success = int.TryParse(productSplits[8],
+                                out int amountPerBox);
+                            if (!success)
+                            {
+                                amountPerBox = 1;
+                            }
+                            product = new BoxedProduct(productId, name, description, new Price()
+                            {
+                                ItemPrice = itemPrice,
+                                Currency = currency
+                            }, maxItemInStock, amountPerBox);
+                    }
+                     
+
+
+                }
+            }
+        }
 
     }
 }
